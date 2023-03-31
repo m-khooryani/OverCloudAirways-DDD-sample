720db43e6ec090f5ab840c57775547147fc74426
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-03-31 19:35:44 +0200

    Add PayInvoiceHandler

---
+++ Payment/Src/OverCloudAirways.Payment.Application/Payments/Policies/Received/PayInvoiceHandler.cs
@@ -0,0 +1,22 @@
+﻿using OverCloudAirways.BuildingBlocks.Application.DomainEventPolicies;
+using OverCloudAirways.BuildingBlocks.Domain.Abstractions;
+using OverCloudAirways.PaymentService.Application.Invoices.Commands.Pay;
+using OverCloudAirways.PaymentService.Domain.Payments.Events;
+
+namespace OverCloudAirways.PaymentService.Application.Payments.Policies.Received;
+
+public class PayInvoiceHandler : IDomainPolicyHandler<PaymentReceivedPolicy, PaymentReceivedDomainEvent>
+{
+    private readonly ICommandsScheduler _commandsScheduler;
+
+    public PayInvoiceHandler(ICommandsScheduler commandsScheduler)
+    {
+        _commandsScheduler = commandsScheduler;
+    }
+
+    public async Task Handle(PaymentReceivedPolicy notification, CancellationToken cancellationToken)
+    {
+        var command = new PayInvoiceCommand(notification.DomainEvent.InvoiceId);
+        await _commandsScheduler.EnqueueAsync(command);
+    }
+}
--- Payment/Tests/OverCloudAirways.PaymentService.Application.UnitTests/Payments/PaymentReceivedPolicyTests.cs
+++ Payment/Tests/OverCloudAirways.PaymentService.Application.UnitTests/Payments/PaymentReceivedPolicyTests.cs
@@ -1,5 +1,6 @@
 ﻿using NSubstitute;
 using OverCloudAirways.BuildingBlocks.Domain.Abstractions;
+using OverCloudAirways.PaymentService.Application.Invoices.Commands.Pay;
 using OverCloudAirways.PaymentService.Application.Payments.Commands.ProjectReadModel;
 using OverCloudAirways.PaymentService.Application.Payments.Policies.Received;
 using OverCloudAirways.PaymentService.TestHelpers.Payments;
@@ -24,5 +25,22 @@ public class PaymentReceivedPolicyTests
         await commandsScheduler
             .Received(1)
             .EnqueueAsync(Arg.Is<ProjectPaymentReadModelCommand>(c => c.PaymentId == policy.DomainEvent.PaymentId));
+    }
+
+    [Fact]
+    public async Task PayInvoiceHandler_Should_Enqueue_PayInvoiceCommand()
+    {
+        // Arrange
+        var commandsScheduler = Substitute.For<ICommandsScheduler>();
+        var handler = new PayInvoiceHandler(commandsScheduler);
+        var policy = new PaymentReceivedPolicyBuilder().Build();
+
+        // Act
+        await handler.Handle(policy, CancellationToken.None);
+
+        // Assert
+        await commandsScheduler
+            .Received(1)
+            .EnqueueAsync(Arg.Is<PayInvoiceCommand>(c => c.InvoiceId == policy.DomainEvent.InvoiceId));
     }
 }
