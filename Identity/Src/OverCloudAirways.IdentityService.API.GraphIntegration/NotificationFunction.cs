a912ee1ac35f5efac57a12c803111d98ced871d8
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-05-16 18:07:13 +0200

    Add Authentication to the exposed API

--- Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/NotificationFunction.cs
+++ Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/NotificationFunction.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace OverCloudAirways.IdentityService.API.GraphIntegration;
@@ -8,24 +10,64 @@ namespace OverCloudAirways.IdentityService.API.GraphIntegration;
 public class NotificationFunction
 {
     private readonly ILogger _logger;
+    private readonly IConfiguration _configuration;
 
     public NotificationFunction(
-        ILoggerFactory loggerFactory)
+        ILoggerFactory loggerFactory,
+        IConfiguration configuration)
     {
         _logger = loggerFactory.CreateLogger<NotificationFunction>();
+        _configuration = configuration;
     }
 
     [Function("notifications")]
-    public IActionResult Run(
+    public async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        
-        foreach (var header in req.Headers)
+
+        if (!IsAuthorized(req))
         {
-            _logger.LogInformation($"Header: {header.Key} = {string.Join(", ", header.Value)}");
+            _logger.LogInformation("Provided credentials are invalid.");
+            return new UnauthorizedResult();
         }
 
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        _logger.LogInformation($"Request Body: {requestBody}");
+
         return new OkResult();
+    }
+
+    private bool IsAuthorized(HttpRequestData req)
+    {
+        var username = _configuration["AuthUsername"];
+        var password = _configuration["AuthPassword"];
+
+        if (req.Headers.TryGetValues("Authorization", out var authHeaders))
+        {
+            var authHeader = authHeaders.FirstOrDefault()?["Basic ".Length..].Trim();
+            if (authHeader is null)
+            {
+                return false;
+            }
+            byte[] decodedBytes = Convert.FromBase64String(authHeader);
+            var decodedText = Encoding.UTF8.GetString(decodedBytes);
+
+            string[] parts = decodedText.Split(':');
+            string providedUsername = parts[0];
+            string providedPassword = parts[1];
+
+            if (username == providedUsername && password == providedPassword)
+            {
+                _logger.LogInformation("Provided credentials are valid.");
+                return true;
+            }
+            else
+            {
+                _logger.LogInformation("Provided credentials are invalid.");
+                return false;
+            }
+        }
+        return false;
     }
 }
