bf3197089008c68da199cf371d96e8f77629f5c2
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-05-17 19:49:19 +0200

    Add Startup class

--- Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Function.cs
+++ Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Function.cs
@@ -1,38 +1,37 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
-namespace OverCloudAirways.IdentityService.API.GraphIntegration
+namespace OverCloudAirways.IdentityService.API.GraphIntegration;
+
+public class Function
 {
-    public class Function
+    private readonly ILogger _logger;
+
+    public Function(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
-
-        public Function(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<Function>();
-        }
-
-        [Function("Function")]
-        public void Run([TimerTrigger("*/5 * * * *")] MyInfo myTimer)
-        {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-        }
+        _logger = loggerFactory.CreateLogger<Function>();
     }
 
-    public class MyInfo
+    [Function("Function")]
+    public void Run([TimerTrigger("*/5 * * * *")] MyInfo myTimer)
     {
-        public MyScheduleStatus ScheduleStatus { get; set; }
-
-        public bool IsPastDue { get; set; }
+        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
     }
+}
 
-    public class MyScheduleStatus
-    {
-        public DateTime Last { get; set; }
+public class MyInfo
+{
+    public MyScheduleStatus ScheduleStatus { get; set; }
 
-        public DateTime Next { get; set; }
+    public bool IsPastDue { get; set; }
+}
 
-        public DateTime LastUpdated { get; set; }
-    }
+public class MyScheduleStatus
+{
+    public DateTime Last { get; set; }
+
+    public DateTime Next { get; set; }
+
+    public DateTime LastUpdated { get; set; }
 }
---
+++ Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+namespace MyNamespace;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddHttpClient();
+
+        //builder.Services.AddSingleton<IMyService>((s) => {
+        //    return new MyService();
+        //});
+
+        //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+    }
+}
