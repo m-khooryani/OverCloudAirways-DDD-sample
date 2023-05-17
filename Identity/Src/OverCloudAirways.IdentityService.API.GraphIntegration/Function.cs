fd83316b91f1b3d528bcdd78ee256fa3f1032ea3
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-05-17 18:50:54 +0200

    Add Serilog

---
+++ Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Function.cs
@@ -0,0 +1,38 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace OverCloudAirways.IdentityService.API.GraphIntegration
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function>();
+        }
+
+        [Function("Function")]
+        public void Run([TimerTrigger("*/5 * * * *")] MyInfo myTimer)
+        {
+            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
--- Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Program.cs
+++ Identity/Src/OverCloudAirways.IdentityService.API.GraphIntegration/Program.cs
@@ -1,12 +1,29 @@
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OverCloudAirways.IdentityService.API.GraphIntegration;
+using Serilog;
+using Serilog.Events;
 
 var host = new HostBuilder()
     .ConfigureAppConfiguration((hostingContext, configurationBuilder) =>
     {
         configurationBuilder
             .AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
+        var appConfig = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+        var instrumentationKey = appConfig.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
+        var logConfig = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.ApplicationInsights(new TelemetryConfiguration(instrumentationKey), TelemetryConverter.Traces);
+
+        //var loggerFactory = services.BuildServiceProvider().GetRequiredService<ILoggerFactory>();
+        services.AddLogging(o => o.AddSerilog(logConfig.CreateLogger(), dispose: true));
     })
     .ConfigureFunctionsWorkerDefaults()
     .Build();
