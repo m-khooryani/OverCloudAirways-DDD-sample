65a817d4def2b2c760d3c60c0aa250492a0bde15
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-06-10 16:57:54 +0200

    Add OverCloudAirways.PaymentService.API

---
+++ Payment/Src/OverCloudAirways.PaymentService.API/Function.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace OverCloudAirways.PaymentService.API
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function>();
+        }
+
+        [Function("Function")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
