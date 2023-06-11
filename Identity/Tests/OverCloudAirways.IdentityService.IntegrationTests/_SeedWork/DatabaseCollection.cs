14501126a57b227a697d9c3b8f77a7d0ac2a2717
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-06-11 12:54:41 +0200

    Fix tests

--- Identity/Tests/OverCloudAirways.IdentityService.IntegrationTests/_SeedWork/DatabaseCollection.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xunit;
-
-namespace OverCloudAirways.IdentityService.IntegrationTests._SeedWork;
-
-[CollectionDefinition("Database collection")]
-public class DatabaseCollection : ICollectionFixture<TestFixture>
-{
-}
