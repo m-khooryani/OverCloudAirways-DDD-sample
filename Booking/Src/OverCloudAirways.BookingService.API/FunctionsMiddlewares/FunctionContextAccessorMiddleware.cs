c59dd5d417203fe15fa1c91280a2be2cfa20aaea
sm-alavian <alavyonline@gmail.com>
2023-06-11 12:40:28 +0330

    add middleware to booking project

---
+++ Booking/Src/OverCloudAirways.BookingService.API/FunctionsMiddlewares/FunctionContextAccessorMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using OverCloudAirways.BuildingBlocks.Domain.Abstractions;
+
+namespace OverCloudAirways.BookingService.API.FunctionsMiddlewares;
+
+public class FunctionContextAccessorMiddleware : IFunctionsWorkerMiddleware
+{
+    private UserAccessor FunctionContextAccessor { get; }
+
+    public FunctionContextAccessorMiddleware(IUserAccessor accessor)
+    {
+        FunctionContextAccessor = accessor as UserAccessor;
+    }
+
+    public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+    {
+        if (FunctionContextAccessor.FunctionContext != null)
+        {
+            // This should never happen because the context should be localized to the current Task chain.
+            // But if it does happen (perhaps the implementation is bugged), then we need to know immediately so it can be fixed.
+            throw new InvalidOperationException($"Unable to initalize {nameof(IUserAccessor)}: context has already been initialized.");
+        }
+
+        FunctionContextAccessor.FunctionContext = context;
+
+        return next(context);
+    }
+}
