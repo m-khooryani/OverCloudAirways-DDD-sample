e758a9c5e4e56cb36a8eab0b0b379718ca5a66ef
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-06-12 18:56:39 +0200

    Add TicketFunctions

---
+++ Booking/Src/OverCloudAirways.BookingService.API/Functions/Tickets/IssueTicketRequest.cs
@@ -0,0 +1,11 @@
+﻿using OverCloudAirways.BookingService.Domain.Customers;
+using OverCloudAirways.BookingService.Domain.Flights;
+
+namespace OverCloudAirways.BookingService.API.Functions.Tickets;
+
+public class IssueTicketRequest
+{
+    public FlightId FlightId { get; init; }
+    public CustomerId CustomerId { get; init; }
+}
+
---
+++ Booking/Src/OverCloudAirways.BookingService.API/Functions/Tickets/TicketFunctions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using OverCloudAirways.BookingService.API.FunctionsMiddlewares;
+using OverCloudAirways.BookingService.Application.Tickets.Commands.Issue;
+using OverCloudAirways.BookingService.Domain.Tickets;
+using OverCloudAirways.BuildingBlocks.Domain.Abstractions;
+using OverCloudAirways.BuildingBlocks.Infrastructure;
+
+namespace OverCloudAirways.BookingService.API.Functions.Tickets;
+
+public class TicketFunctions
+{
+    private readonly CqrsInvoker _cqrsInvoker;
+    private readonly IJsonSerializer _jsonSerializer;
+
+    public TicketFunctions(
+        CqrsInvoker cqrsInvoker,
+        IJsonSerializer jsonSerializer)
+    {
+        _cqrsInvoker = cqrsInvoker;
+        _jsonSerializer = jsonSerializer;
+    }
+
+    [Function("issue-ticket")]
+    [Authorized("AirlineStaff")]
+    public async Task IssueTicket(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "users")] HttpRequestData req)
+    {
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var request = _jsonSerializer.Deserialize<IssueTicketRequest>(requestBody);
+
+        var registerUserCommand = new IssueTicketCommand(
+            TicketId.New(),
+            request.FlightId,
+            request.CustomerId);
+        await _cqrsInvoker.CommandAsync(registerUserCommand);
+    }
+}
