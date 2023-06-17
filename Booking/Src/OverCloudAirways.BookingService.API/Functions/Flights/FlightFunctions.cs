c37d83bf5ede99a69760f22ea206f5175a9a700b
Mojtaba Khooryani <m.khooryani@outlook.com>
2023-06-17 11:59:54 +0200

    Add ScheduleFlight function

---
+++ Booking/Src/OverCloudAirways.BookingService.API/Functions/Flights/FlightFunctions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using OverCloudAirways.BookingService.API.Functions.Flights.Requests;
+using OverCloudAirways.BookingService.API.FunctionsMiddlewares;
+using OverCloudAirways.BookingService.Application.Flights.Commands.Schedule;
+using OverCloudAirways.BookingService.Domain.Flights;
+using OverCloudAirways.BuildingBlocks.Domain.Abstractions;
+using OverCloudAirways.BuildingBlocks.Infrastructure;
+
+namespace OverCloudAirways.BookingService.API.Functions.Flights;
+
+public class FlightFunctions
+{
+    private readonly CqrsInvoker _cqrsInvoker;
+    private readonly IJsonSerializer _jsonSerializer;
+
+    public FlightFunctions(
+        CqrsInvoker cqrsInvoker,
+        IJsonSerializer jsonSerializer)
+    {
+        _cqrsInvoker = cqrsInvoker;
+        _jsonSerializer = jsonSerializer;
+    }
+
+    [Function("schedule-flight")]
+    [Authorized("AirlineStaff")]
+    public async Task IssueTicket(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "flights")] HttpRequestData req)
+    {
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var request = _jsonSerializer.Deserialize<ScheduleFlightRequest>(requestBody);
+
+        var command = new ScheduleFlightCommand(
+            FlightId.New(),
+            request.Number,
+            request.DepartureAirportId,
+            request.DepartureAirportId,
+            request.DepartureTime,
+            request.ArrivalTime,
+            request.Route,
+            request.Distance,
+            request.AircraftId,
+            request.AvailableSeats,
+            request.MaximumLuggageWeight);
+        await _cqrsInvoker.CommandAsync(command);
+    }
+}
---
+++ Booking/Src/OverCloudAirways.BookingService.API/Functions/Flights/Requests/ScheduleFlightRequest.cs
@@ -0,0 +1,18 @@
+﻿using OverCloudAirways.BookingService.Domain.Aircrafts;
+using OverCloudAirways.BookingService.Domain.Airports;
+
+namespace OverCloudAirways.BookingService.API.Functions.Flights.Requests;
+
+public class ScheduleFlightRequest
+{
+    public string Number { get; init; }
+    public AirportId DepartureAirportId { get; init; }
+    public AirportId DestinationAirportId { get; init; }
+    public DateTimeOffset DepartureTime { get; init; }
+    public DateTimeOffset ArrivalTime { get; init; }
+    public string Route { get; init; }
+    public int Distance { get; init; }
+    public AircraftId AircraftId { get; init; }
+    public int AvailableSeats { get; init; }
+    public double MaximumLuggageWeight { get; init; }
+}
